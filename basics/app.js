76d25296eec66a0bd304926d71a6ff4614b5d5da
Thisisyusub <kanan.yusub@gmail.com>
2022-02-06 20:09:21 +0400

    basics completed

--- basics/app.js
+++ basics/app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 /// works for every incoming requests
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-type', 'text/html');
+
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.JS</h1></body>');
+    res.write('</html>');
+
+    res.end();
 });
 
 server.listen(3000);
