1ec372717caa70fef2196851d9de6cd59d3aecb3
Thisisyusub <kanan.yusub@gmail.com>
2022-02-06 20:30:44 +0400

    changed

--- npm-intro/info.js
+++ npm-intro/info.js
@@ -2,6 +2,9 @@ const requestHandler = (req, res) => {
     res.write('Topics\n');
     res.write('npm init / npm init -y\n');
     res.write('start command\n');
+    res.write('npm run custom-script\n');
+    res.write('nodemon\n');
+    res.write('npm install package-name --save --save-dev -g\n');
 
     res.end();
 }
